f970d907001b182b3ac62afca9c590398eb95b6f
cleven <clevengodsontech@gmail.com>
2026-09-04 08:40:57 +0300

    chore: init commit -restructure cms

---
+++ main/apps/destinations/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'destinations'
+
+urlpatterns = [
+    path('', views.destination_list, name='list'),
+    path('<slug:slug>/', views.destination_detail, name='detail'),
+    path('category/<slug:slug>/', views.category_detail, name='category'),
+]
