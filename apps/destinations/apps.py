8a27b7e679010bd6cc5f17cdcdcf61889d11caca
cleven <clevengodsontech@gmail.com>
2026-09-05 21:07:13 +0300

    resolve: file restruct

--- apps/destinations/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class DestinationsConfig(AppConfig):
-    name = 'apps.destinations'
-
-    def ready(self):
-        # Wire Google self-indexing for destinations (seo-destination skill output)
-        import apps.destinations.signals  # noqa
